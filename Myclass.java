69dd2ce447a4f1665f0e1371eeea2f9dd2b954c1
Akash R Acharya <akaashracharya@gmail.com>
2025-08-27 13:29:35 +0530

    initial

--- Myclass.java
+++ Myclass.java
@@ -3,10 +3,10 @@ public class Myclass{
     public static void main(String args[]){
         int[] arr = {1,2,3,4,5,6,7};
         int target = 7;
-        int res = binary(arr, 0, arr.length-1,target);
+        int res = binaryrec(arr, 0, arr.length-1,target);
         System.out.println(res);
 }
-    public static int binary(int[] arr , int s , int e, int target){
+    public static int binaryrec(int[] arr , int s , int e, int target){
         if(s > e){
             return -1;
         }
@@ -15,8 +15,8 @@ public class Myclass{
             return mid;
         }
         if( arr[mid] < target){
-            return binary(arr, mid+1 , e, target);
+            return binaryrec(arr, mid+1 , e, target);
         }
-        return binary(arr, s, mid-1, target);
+        return binaryrec(arr, s, mid-1, target);
     }
 }
